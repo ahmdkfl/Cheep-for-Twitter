ad80f0e5d452a04729ce63a19f9cedd4a562f527
Kafeel <ahmadkafeel94@gmail.com>
2019-02-12 00:36:07 +0000

    Added user profile info

--- lib/main.dart
+++ lib/main.dart
@@ -211,8 +211,10 @@ class TabBarHome extends StatelessWidget {
             ),
           body: TabBarView(
             children: [
-              new Container(color: Colors.red),
-              new Container(color: Colors.blue),
+              new Container(color: Colors.green),
+              new Container(color: Colors.white, 
+                child: getUserProfile(client)
+              ),
               new Container(color: Colors.green)
             ],
           ),
@@ -247,4 +249,72 @@ Future<oauth1.Credentials> getCredentials2() async {
 	String cred = prefs.getString("credentials");
   oauth1.Credentials  e = new oauth1.Credentials.fromJSON(cred);
   return e;
+}
+
+Future<dynamic> getUserInfo(client){
+  return client.get('https://api.twitter.com/1.1/account/verify_credentials.json').then((res) {
+      return res;
+  });
+}
+
+Future<dynamic> getUserTimeline(client){
+  return client.get('https://api.twitter.com/1.1/statuses/home_timeline.json').then((res) {
+      return res;
+  });
+}
+
+// Second Tab for the user profile
+getUserProfile(client){
+  return LayoutBuilder(
+    builder: (BuildContext context, BoxConstraints viewportConstraints) {
+      return SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: viewportConstraints.maxHeight,
+          ),
+          child: IntrinsicHeight(
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: FutureBuilder(
+                    future: getUserInfo(client),
+                    builder: (context, snapshot){
+                      Map<String, dynamic> data = json.decode(snapshot.data.body);
+                      
+                      return Container(child:
+                        Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Card(child:
+                              Padding(padding: EdgeInsets.all(9),child:
+                                Column(crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    ClipOval(
+                                      child: Image.network(
+                                          data['profile_image_url'].replaceAll(new RegExp(r'normal'), '200x200'),
+                                          height: 100,
+                                          width: 100,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    Text("@"+data['screen_name']),
+                                    Text(data['name'], style: new TextStyle(fontWeight: FontWeight.bold)),
+                                    Text(data['followers_count'].toString()+" Followers"),
+                                    Text(data['friends_count'].toString()+ " Following")
+                                    ]
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+  );
 }
