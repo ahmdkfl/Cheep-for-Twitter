23421d6df6d8d228d92e71d7d02f61f0bf8dc084
Kafeel <ahmadkafeel94@gmail.com>
2019-02-26 19:28:28 +0000

    Added Twitter authorization page

---
+++ lib/pages/link_page.dart
@@ -0,0 +1,24 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+
+class LinkPage extends StatelessWidget {
+
+  final String address;
+
+  LinkPage({Key key, @required this.address}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    var authorizePage = new WebviewScaffold(
+      url: address,
+      appBar: new AppBar(
+        title: new Text("Cheep Login"),
+      ),
+    );
+
+    return authorizePage;
+  }
+}
