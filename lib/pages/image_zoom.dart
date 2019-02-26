403d967f8d231e79dbc73b97b1da358df1bcc915
Kafeel <ahmadkafeel94@gmail.com>
2019-02-26 19:29:21 +0000

    Added class to view an image

---
+++ lib/pages/image_zoom.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+
+class ImageZoom extends StatelessWidget {
+
+  final String url;
+
+  ImageZoom({Key key, @required this.url}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(),
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: url,
+            child: CachedNetworkImage(
+              placeholder: CircularProgressIndicator(),
+              imageUrl:url
+            ),
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
