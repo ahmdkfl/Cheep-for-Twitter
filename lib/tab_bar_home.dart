80143406fc108a174149df0335b07255fdc16f0e
Kafeel <ahmadkafeel94@gmail.com>
2019-04-22 21:43:20 +0100

    Added 'Tweet' button and formatted the code

--- lib/tab_bar_home.dart
+++ lib/tab_bar_home.dart
@@ -4,9 +4,9 @@ import 'package:cheep_for_twitter/twitterapi.dart';
 import 'package:cheep_for_twitter/pages/user_profile.dart';
 import 'package:cheep_for_twitter/pages/home_timeline.dart';
 import 'package:cheep_for_twitter/pages/settings.dart';
+import 'package:cheep_for_twitter/pages/new_tweet.dart';
 
 class TabBarHome extends StatefulWidget {
-
   var tokens;
 
   TabBarHome(this.tokens);
@@ -16,16 +16,15 @@ class TabBarHome extends StatefulWidget {
 }
 
 class _TabBarHomeState extends State<TabBarHome> {
-
   var client, api = Twitterapi();
 
   @override
-  void initState(){
+  void initState() {
     var r = widget.tokens;
     var r2 = r.split('=');
     var r3 = r2[1].split('&');
-    var oauth_token_sec=r2[2];
-    var oauth_token=r3[0];
+    var oauth_token_sec = r2[2];
+    var oauth_token = r3[0];
     client = api.getAuthorClient(oauth_token, oauth_token_sec);
     super.initState();
   }
@@ -35,30 +34,46 @@ class _TabBarHomeState extends State<TabBarHome> {
     return MaterialApp(
       home: DefaultTabController(
         length: 3,
-        child: Scaffold(appBar: AppBar(title: const Text("Cheep for Twitter"), centerTitle: true,),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Cheep for Twitter"),
+            centerTitle: true,
+            actions: <Widget>[
+              FlatButton(
+                child: Text(
+                  "Tweet",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return NewTweet(client: client);
+                  }));
+                },
+              )
+            ],
+          ),
           bottomNavigationBar: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.perm_identity)),
-                Tab(icon: Icon(Icons.settings)),
-              ],
-              labelColor: Colors.blue,
-              unselectedLabelColor: Colors.lightBlueAccent,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorPadding: EdgeInsets.all(5.0),
-              indicatorColor: Colors.blue,
-            ),
+            tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.perm_identity)),
+              Tab(icon: Icon(Icons.settings)),
+            ],
+            labelColor: Colors.blue,
+            unselectedLabelColor: Colors.lightBlueAccent,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorPadding: EdgeInsets.all(5.0),
+            indicatorColor: Colors.blue,
+          ),
           body: TabBarView(
             children: [
-              new Container(color: Colors.white,
-                child: HomeTimeline(client: client,)
-                ),
-              new Container(color: Colors.white, 
-                child: UserProfile(client: client)
-                ),
-              new Container(color: Colors.white,
-                child: Settings()
-                )
+              new Container(
+                  color: Colors.white,
+                  child: HomeTimeline(
+                    client: client,
+                  )),
+              new Container(
+                  color: Colors.white, child: UserProfile(client: client)),
+              new Container(color: Colors.white, child: Settings())
             ],
           ),
         ),
