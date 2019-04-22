97b87260b896d78fc9e86e7edcdb930e384e7bfd
Kafeel <ahmadkafeel94@gmail.com>
2019-04-23 00:45:46 +0100

    appBar to SliverAppBar with profile iamge

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -28,88 +28,193 @@ class ProfileState extends State<Profile> with AutomaticKeepAliveClientMixin {
     super.initState();
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   Map<String, dynamic> data = widget.user;
+  //   return Scaffold(
+  //     appBar: new AppBar(
+  //       title: Text(data['name'], style: TextStyle(color: Colors.white)),
+  //       centerTitle: true,
+  //     ),
+  //     body: ListView(
+  //       shrinkWrap: true,
+  //       children: <Widget>[
+  //         Container(
+  //             child: Container(
+  //                 child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.stretch,
+  //           children: <Widget>[
+  //             Card(
+  //               child: Padding(
+  //                 padding: EdgeInsets.all(9),
+  //                 child: Center(
+  //                     child: Column(
+  //                   children: <Widget>[
+  //                     Column(
+  //                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                         children: <Widget>[
+  //                           ClipOval(
+  //                             child: Image.network(
+  //                               data['profile_image_url'].replaceAll(
+  //                                   new RegExp(r'normal'), '200x200'),
+  //                               height: 100,
+  //                               width: 100,
+  //                               fit: BoxFit.cover,
+  //                             ),
+  //                           ),
+  //                           Text("@" + data['screen_name']),
+  //                           Text(data['name'],
+  //                               style:
+  //                                   new TextStyle(fontWeight: FontWeight.bold)),
+  //                         ]),
+  //                     Row(
+  //                         mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //                         children: <Widget>[
+  //                           Text(data['followers_count'].toString() +
+  //                               " Followers"),
+  //                           Text(
+  //                               data['friends_count'].toString() + " Following")
+  //                         ])
+  //                   ],
+  //                 )),
+  //               ),
+  //             ),
+  //           ],
+  //         ))),
+  //         Container(
+  //             child: FutureBuilder(
+  //           future: _loadUserTime,
+  //           builder: (context, snapshot) {
+  //             if (snapshot.connectionState == ConnectionState.done) {
+  //               List<dynamic> userTweets = json.decode(snapshot.data.body);
+
+  //               List<Widget> list = new List<Widget>();
+  //               if (_cachedTweets.isEmpty) {
+  //                 userTweets.forEach((tweet) {
+  //                   var t = Tweet.fromJson(tweet);
+  //                   TweetCard r = TweetCard(tweet: t);
+  //                   list.add(GestureDetector(
+  //                     child: r,
+  //                     onTap: () {
+  //                       Navigator.push(context, MaterialPageRoute(builder: (_) {
+  //                         return TweetDetails(tweet: t);
+  //                       }));
+  //                     },
+  //                   ));
+  //                 });
+  //                 _cachedTweets = list;
+  //               }
+  //               return new Column(children: _cachedTweets);
+  //             } else
+  //               return Center(child: CircularProgressIndicator());
+  //           },
+  //         ))
+  //       ],
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     Map<String, dynamic> data = widget.user;
     return Scaffold(
-      appBar: new AppBar(
-        title: Text(data['name'], style: TextStyle(color: Colors.white)),
-        centerTitle: true,
-      ),
-      body: ListView(
-        shrinkWrap: true,
-        children: <Widget>[
-          Container(
-              child: Container(
-                  child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Card(
-                child: Padding(
-                  padding: EdgeInsets.all(9),
-                  child: Center(
-                      child: Column(
-                    children: <Widget>[
-                      Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            ClipOval(
-                              child: Image.network(
-                                data['profile_image_url'].replaceAll(
-                                    new RegExp(r'normal'), '200x200'),
-                                height: 100,
-                                width: 100,
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                            Text("@" + data['screen_name']),
-                            Text(data['name'],
-                                style:
-                                    new TextStyle(fontWeight: FontWeight.bold)),
-                          ]),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            Text(data['followers_count'].toString() +
-                                " Followers"),
-                            Text(
-                                data['friends_count'].toString() + " Following")
-                          ])
-                    ],
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(data['name'],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      )),
+                  background: Image.network(
+                    data['profile_banner_url'],
+                    fit: BoxFit.cover,
                   )),
+            ),
+          ];
+        },
+        body: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            Container(
+                child: Container(
+                    child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(9),
+                    child: Center(
+                        child: Column(
+                      children: <Widget>[
+                        Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              ClipOval(
+                                child: Image.network(
+                                  data['profile_image_url'].replaceAll(
+                                      new RegExp(r'normal'), '200x200'),
+                                  height: 100,
+                                  width: 100,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Text("@" + data['screen_name']),
+                              Text(data['name'],
+                                  style: new TextStyle(
+                                      fontWeight: FontWeight.bold)),
+                            ]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              Text(data['followers_count'].toString() +
+                                  " Followers"),
+                              Text(data['friends_count'].toString() +
+                                  " Following")
+                            ])
+                      ],
+                    )),
+                  ),
                 ),
-              ),
-            ],
-          ))),
-          Container(
-              child: FutureBuilder(
-            future: _loadUserTime,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                List<dynamic> userTweets = json.decode(snapshot.data.body);
+              ],
+            ))),
+            Container(
+                child: FutureBuilder(
+              future: _loadUserTime,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  List<dynamic> userTweets = json.decode(snapshot.data.body);
 
-                List<Widget> list = new List<Widget>();
-                if (_cachedTweets.isEmpty) {
-                  userTweets.forEach((tweet) {
-                    var t = Tweet.fromJson(tweet);
-                    TweetCard r = TweetCard(tweet: t);
-                    list.add(GestureDetector(
-                      child: r,
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (_) {
-                          return TweetDetails(tweet: t);
-                        }));
-                      },
-                    ));
-                  });
-                  _cachedTweets = list;
-                }
-                return new Column(children: _cachedTweets);
-              } else
-                return Center(child: CircularProgressIndicator());
-            },
-          ))
-        ],
+                  List<Widget> list = new List<Widget>();
+                  if (_cachedTweets.isEmpty) {
+                    userTweets.forEach((tweet) {
+                      var t = Tweet.fromJson(tweet);
+                      TweetCard r = TweetCard(tweet: t);
+                      list.add(GestureDetector(
+                        child: r,
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_) {
+                            return TweetDetails(tweet: t);
+                          }));
+                        },
+                      ));
+                    });
+                    _cachedTweets = list;
+                  }
+                  return new Column(children: _cachedTweets);
+                } else
+                  return Center(child: CircularProgressIndicator());
+              },
+            ))
+          ],
+        ),
       ),
     );
   }
