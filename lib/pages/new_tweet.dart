7b29df740444e601c72566662ae2a434eb93765f
Kafeel <ahmadkafeel94@gmail.com>
2019-04-22 21:42:21 +0100

    Added ability to tweet

---
+++ lib/pages/new_tweet.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class NewTweet extends StatefulWidget {
+  var client;
+
+  NewTweet({Key, key, @required this.client}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => NewTweetState();
+}
+
+class NewTweetState extends State<NewTweet> {
+  var tweetTextFieldController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New Tweet", style: TextStyle(color: Colors.white)),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            TextField(
+                controller: tweetTextFieldController,
+                maxLength: 280,
+                decoration: InputDecoration(border: OutlineInputBorder())),
+            FlatButton(
+              child: Text("Tweet", style: TextStyle(color: Colors.white)),
+              color: Colors.blue,
+              onPressed: _sendTweet,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _sendTweet() {
+    var tweet = new Map();
+    tweet['status'] = tweetTextFieldController.text.toString();
+    widget.client
+        .post('https://api.twitter.com/1.1/statuses/update.json', body: tweet);
+    Navigator.of(context).pop();
+  }
+}
