d0314cecca5a9dba8c9d126f44dda6089d85ddd0
Kafeel <ahmadkafeel94@gmail.com>
2019-04-23 00:45:19 +0100

    Change appBar from static to reactive to scrolling AppBar to SliverAppBar

--- lib/tab_bar_home.dart
+++ lib/tab_bar_home.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:cheep_for_twitter/twitterapi.dart';
 import 'package:cheep_for_twitter/pages/user_profile.dart';
+import 'package:cheep_for_twitter/pages/profile.dart';
 import 'package:cheep_for_twitter/pages/home_timeline.dart';
 import 'package:cheep_for_twitter/pages/settings.dart';
 import 'package:cheep_for_twitter/pages/new_tweet.dart';
@@ -15,7 +18,8 @@ class TabBarHome extends StatefulWidget {
   State<StatefulWidget> createState() => _TabBarHomeState();
 }
 
-class _TabBarHomeState extends State<TabBarHome> {
+class _TabBarHomeState extends State<TabBarHome>
+    with AutomaticKeepAliveClientMixin {
   var client, api = Twitterapi();
 
   @override
@@ -29,29 +33,75 @@ class _TabBarHomeState extends State<TabBarHome> {
     super.initState();
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     home: DefaultTabController(
+  //       length: 3,
+  //       child: Scaffold(
+  //         appBar: AppBar(
+  //           title: const Text("Cheep for Twitter"),
+  //           centerTitle: true,
+  //           actions: <Widget>[
+  //             FlatButton(
+  //               child: Text(
+  //                 "Tweet",
+  //                 style: TextStyle(color: Colors.white),
+  //               ),
+  //               onPressed: () {
+  //                 Navigator.push(context, MaterialPageRoute(builder: (_) {
+  //                   return NewTweet(client: client);
+  //                 }));
+  //               },
+  //             )
+  //           ],
+  //         ),
+  //         bottomNavigationBar: TabBar(
+  //           tabs: [
+  //             Tab(icon: Icon(Icons.home)),
+  //             Tab(icon: Icon(Icons.perm_identity)),
+  //             Tab(icon: Icon(Icons.settings)),
+  //           ],
+  //           labelColor: Colors.blue,
+  //           unselectedLabelColor: Colors.lightBlueAccent,
+  //           indicatorSize: TabBarIndicatorSize.label,
+  //           indicatorPadding: EdgeInsets.all(5.0),
+  //           indicatorColor: Colors.blue,
+  //         ),
+  //         body: TabBarView(
+  //           children: [
+  //             new Container(
+  //                 color: Colors.white,
+  //                 child: HomeTimeline(
+  //                   client: client,
+  //                 )),
+  //             // new Container(
+  //             //     color: Colors.white, child: UserProfile(client: client)),
+  //             new Container(
+  //                 color: Colors.white,
+  //                 child: FutureBuilder(
+  //                     future: _getUserInfo(client),
+  //                     builder: (context, snapshot) {
+  //                       if (snapshot.connectionState == ConnectionState.done) {
+  //                         Map<String, dynamic> user =
+  //                             json.decode(snapshot.data.body);
+  //                         return Profile(client: client, user: user);
+  //                       }
+  //                     })),
+  //             new Container(color: Colors.white, child: Settings())
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
-          appBar: AppBar(
-            title: const Text("Cheep for Twitter"),
-            centerTitle: true,
-            actions: <Widget>[
-              FlatButton(
-                child: Text(
-                  "Tweet",
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) {
-                    return NewTweet(client: client);
-                  }));
-                },
-              )
-            ],
-          ),
           bottomNavigationBar: TabBar(
             tabs: [
               Tab(icon: Icon(Icons.home)),
@@ -64,20 +114,66 @@ class _TabBarHomeState extends State<TabBarHome> {
             indicatorPadding: EdgeInsets.all(5.0),
             indicatorColor: Colors.blue,
           ),
-          body: TabBarView(
-            children: [
-              new Container(
-                  color: Colors.white,
-                  child: HomeTimeline(
-                    client: client,
-                  )),
-              new Container(
-                  color: Colors.white, child: UserProfile(client: client)),
-              new Container(color: Colors.white, child: Settings())
-            ],
+          body: new NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                    title: Text("Cheep for Twitter"),
+                    floating: true,
+                    pinned: false,
+                    snap: false,
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text(
+                          "Tweet",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (_) {
+                            return NewTweet(client: client);
+                          }));
+                        },
+                      )
+                    ])
+              ];
+            },
+            body: TabBarView(
+              children: [
+                new Container(
+                    color: Colors.white,
+                    child: HomeTimeline(
+                      client: client,
+                    )),
+                // new Container(
+                //     color: Colors.white, child: UserProfile(client: client)),
+                new Container(
+                    color: Colors.white,
+                    child: FutureBuilder(
+                        future: _getUserInfo(client),
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.done) {
+                            Map<String, dynamic> user =
+                                json.decode(snapshot.data.body);
+                            return Profile(client: client, user: user);
+                          }
+                        })),
+                new Container(color: Colors.white, child: Settings())
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+
+  Future<dynamic> _getUserInfo(client) async {
+    return await client
+        .get('https://api.twitter.com/1.1/account/verify_credentials.json');
   }
 }
