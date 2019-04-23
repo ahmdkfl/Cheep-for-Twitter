1b71b70b1420ce8ddd705b0a92cd0495d906dcc6
Kafeel <ahmadkafeel94@gmail.com>
2019-04-23 21:10:14 +0100

    Get 200 tweets and removed comments

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -28,92 +28,6 @@ class ProfileState extends State<Profile> with AutomaticKeepAliveClientMixin {
     super.initState();
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   Map<String, dynamic> data = widget.user;
-  //   return Scaffold(
-  //     appBar: new AppBar(
-  //       title: Text(data['name'], style: TextStyle(color: Colors.white)),
-  //       centerTitle: true,
-  //     ),
-  //     body: ListView(
-  //       shrinkWrap: true,
-  //       children: <Widget>[
-  //         Container(
-  //             child: Container(
-  //                 child: Column(
-  //           crossAxisAlignment: CrossAxisAlignment.stretch,
-  //           children: <Widget>[
-  //             Card(
-  //               child: Padding(
-  //                 padding: EdgeInsets.all(9),
-  //                 child: Center(
-  //                     child: Column(
-  //                   children: <Widget>[
-  //                     Column(
-  //                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //                         children: <Widget>[
-  //                           ClipOval(
-  //                             child: Image.network(
-  //                               data['profile_image_url'].replaceAll(
-  //                                   new RegExp(r'normal'), '200x200'),
-  //                               height: 100,
-  //                               width: 100,
-  //                               fit: BoxFit.cover,
-  //                             ),
-  //                           ),
-  //                           Text("@" + data['screen_name']),
-  //                           Text(data['name'],
-  //                               style:
-  //                                   new TextStyle(fontWeight: FontWeight.bold)),
-  //                         ]),
-  //                     Row(
-  //                         mainAxisAlignment: MainAxisAlignment.spaceAround,
-  //                         children: <Widget>[
-  //                           Text(data['followers_count'].toString() +
-  //                               " Followers"),
-  //                           Text(
-  //                               data['friends_count'].toString() + " Following")
-  //                         ])
-  //                   ],
-  //                 )),
-  //               ),
-  //             ),
-  //           ],
-  //         ))),
-  //         Container(
-  //             child: FutureBuilder(
-  //           future: _loadUserTime,
-  //           builder: (context, snapshot) {
-  //             if (snapshot.connectionState == ConnectionState.done) {
-  //               List<dynamic> userTweets = json.decode(snapshot.data.body);
-
-  //               List<Widget> list = new List<Widget>();
-  //               if (_cachedTweets.isEmpty) {
-  //                 userTweets.forEach((tweet) {
-  //                   var t = Tweet.fromJson(tweet);
-  //                   TweetCard r = TweetCard(tweet: t);
-  //                   list.add(GestureDetector(
-  //                     child: r,
-  //                     onTap: () {
-  //                       Navigator.push(context, MaterialPageRoute(builder: (_) {
-  //                         return TweetDetails(tweet: t);
-  //                       }));
-  //                     },
-  //                   ));
-  //                 });
-  //                 _cachedTweets = list;
-  //               }
-  //               return new Column(children: _cachedTweets);
-  //             } else
-  //               return Center(child: CircularProgressIndicator());
-  //           },
-  //         ))
-  //       ],
-  //     ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     Map<String, dynamic> data = widget.user;
@@ -224,7 +138,7 @@ class ProfileState extends State<Profile> with AutomaticKeepAliveClientMixin {
 
   Future<dynamic> _getUserTimeline(client) async {
     return await client.get(
-        'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=' +
+        'https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&screen_name=' +
             widget.user['screen_name']);
   }
 }
