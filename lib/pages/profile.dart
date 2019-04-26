5581545c570eebd8870f71d04f925ab820484540
Kafeel <ahmadkafeel94@gmail.com>
2019-04-26 22:47:08 +0100

    Removed client and created a profile card

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,17 +1,17 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:cheep_for_twitter/pages/tweet_details.dart';
 import 'package:cheep_for_twitter/tweet/tweet.dart';
-import 'package:cheep_for_twitter/tweet/tweet_card4.dart';
+import 'package:cheep_for_twitter/tweet/tweet_card.dart';
 import 'package:flutter/material.dart';
-import 'package:collection/collection.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cheep_for_twitter/twitterapi.dart';
 
 class Profile extends StatefulWidget {
-  var client;
   var user;
 
-  Profile({Key, key, @required this.client, @required this.user})
+  Profile({Key, key, @required this.user})
       : super(key: key);
 
   @override
@@ -24,21 +24,56 @@ class ProfileState extends State<Profile> {
 
   @override
   void initState() {
-    _loadUserTime = _getUserTimeline(widget.client);
-    _cachedTweets = List<Widget>();
     super.initState();
+    _loadUserTime = _getUserTimeline();
+    _cachedTweets = List<Widget>();
   }
 
   @override
   Widget build(BuildContext context) {
     Map<String, dynamic> data = widget.user;
+
+    var profileCard = Card(
+      child: Padding(
+        padding: EdgeInsets.all(9),
+        child: Center(
+            child: Column(
+          children: <Widget>[
+            Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  ClipOval(
+                    child: CachedNetworkImage(
+                      imageUrl: data['profile_image_url']
+                          .replaceAll(new RegExp(r'normal'), '200x200'),
+                      errorWidget: (context, url, error) =>
+                          new Icon(Icons.error),
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Text("@" + data['screen_name']),
+                  Text(data['name'],
+                      style: new TextStyle(fontWeight: FontWeight.bold)),
+                ]),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text(data['followers_count'].toString() + " Followers"),
+                  Text(data['friends_count'].toString() + " Following")
+                ])
+          ],
+        )),
+      ),
+    );
     return Scaffold(
       body: NestedScrollView(
         headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
           return <Widget>[
             SliverAppBar(
               expandedHeight: 200.0,
-              floating: false,
+              floating: true,
               pinned: true,
               flexibleSpace: FlexibleSpaceBar(
                   centerTitle: true,
@@ -54,87 +89,50 @@ class ProfileState extends State<Profile> {
             ),
           ];
         },
-        body: ListView(
-          shrinkWrap: true,
+        body: Column(
+          // shrinkWrap: true,
           children: <Widget>[
-            Container(
-                child: Container(
-                    child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Card(
-                  child: Padding(
-                    padding: EdgeInsets.all(9),
-                    child: Center(
-                        child: Column(
-                      children: <Widget>[
-                        Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              ClipOval(
-                                child: CachedNetworkImage(
-                                  imageUrl: data['profile_image_url']
-                                      .replaceAll(
-                                          new RegExp(r'normal'), '200x200'),
-                                  errorWidget: (context, url, error) =>
-                                      new Icon(Icons.error),
-                                  height: 100,
-                                  width: 100,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Text("@" + data['screen_name']),
-                              Text(data['name'],
-                                  style: new TextStyle(
-                                      fontWeight: FontWeight.bold)),
-                            ]),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: <Widget>[
-                              Text(data['followers_count'].toString() +
-                                  " Followers"),
-                              Text(data['friends_count'].toString() +
-                                  " Following")
-                            ])
-                      ],
-                    )),
-                  ),
-                ),
-              ],
-            ))),
-            Container(
-                child: FutureBuilder(
+            FutureBuilder(
               future: _loadUserTime,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   List<dynamic> userTweets = json.decode(snapshot.data.body);
 
                   List<Widget> list = new List<Widget>();
+                  list.add(profileCard);
                   userTweets.forEach((tweet) {
                     var t = Tweet.fromJson(tweet);
-                    TweetCard r = TweetCard(tweet: t, client: widget.client);
+                    TweetCard r = TweetCard(tweet: t);
                     list.add(GestureDetector(
                       child: r,
                       onTap: () {
                         Navigator.push(context, MaterialPageRoute(builder: (_) {
-                          return TweetDetails(tweet: t);
+                          return TweetDetails(
+                            tweet: t
+                          );
                         }));
                       },
                     ));
                   });
                   _cachedTweets = list;
-                  return new Column(children: _cachedTweets);
+                  return Expanded(child:
+                      StreamBuilder<Object>(builder: (context, snapshot) {
+                    return ListView(children: _cachedTweets, shrinkWrap: true);
+                  }));
                 } else
-                  return Container(child: CircularProgressIndicator());
+                  return Align(
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator());
               },
-            ))
+            )
           ],
         ),
       ),
     );
   }
 
-  Future<dynamic> _getUserTimeline(client) async {
+  Future<dynamic> _getUserTimeline() async {
+    var client = Twitterapi().getClient();
     return await client.get(
         'https://api.twitter.com/1.1/statuses/user_timeline.json?count=200&screen_name=' +
             widget.user['screen_name']);
