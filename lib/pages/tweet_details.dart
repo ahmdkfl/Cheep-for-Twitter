5781c9ab0a2662ff0648db8bd88bb88e6c6688e3
Kafeel <ahmadkafeel94@gmail.com>
2019-04-26 23:55:47 +0100

    Simplified the retrieval of the replies

--- lib/pages/tweet_details.dart
+++ lib/pages/tweet_details.dart
@@ -10,8 +10,7 @@ import 'dart:convert';
 class TweetDetails extends StatelessWidget {
   final Tweet tweet;
 
-  TweetDetails({Key key, @required this.tweet})
-      : super(key: key);
+  TweetDetails({Key key, @required this.tweet}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +55,13 @@ class TweetDetails extends StatelessWidget {
                           },
                         );
                       });
-                } else
+                } else if(snapshot.data == null)
+                  return Column(
+                    children: <Widget>[
+                      Text("Limit reached!"),
+                    ],
+                  );
+                  else
                   return Column(children: <Widget>[
                     Align(
                         alignment: Alignment.center,
@@ -68,37 +73,14 @@ class TweetDetails extends StatelessWidget {
         ));
   }
 
-  Future<dynamic> _getReplies(client, id, sinceId) {
+  Future<Map<String, dynamic>> _loadReplies() async {
     var client = Twitterapi().getClient();
-    return client
-        .get('https://api.twitter.com/1.1/search/tweets.json?count=100&q=' +
-            id.toString() +
+    var result = await client.get(
+        'https://api.twitter.com/1.1/search/tweets.json?count=100&q=' +
+            tweet.user['screen_name'].toString() +
             "&since_id=" +
-            sinceId.toString())
-        .then((res) {
-      return res;
-    });
-  }
-
-  Future<Map<String, dynamic>> _loadReplies() {
-    return _getCredentials().then((credentials) {
-      var r = credentials;
-      var r2 = r.split('=');
-      var r3 = r2[1].split('&');
-      var oauth_token_sec = r2[2];
-      var oauth_token = r3[0];
-      var client = Twitterapi().getAuthorClient(oauth_token, oauth_token_sec);
-      return _getReplies(client, tweet.user['screen_name'], tweet.idStr)
-          .then((result) {
-        Map<String, dynamic> data = json.decode(result.body);
-        return data;
-      });
-    });
-  }
-
-  Future<String> _getCredentials() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    String cred = prefs.getString("credentials");
-    return cred;
+            tweet.idStr.toString());
+    Map<String, dynamic> data = json.decode(result.body);
+    return data;
   }
 }
