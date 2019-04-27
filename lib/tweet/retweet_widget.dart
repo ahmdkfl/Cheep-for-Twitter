9887da6af22e4a2510a5b265fa119dc637719e5c
Kafeel <ahmadkafeel94@gmail.com>
2019-04-27 02:15:36 +0100

    Retweet implemented without quoted retweets

--- lib/tweet/retweet_widget.dart
+++ lib/tweet/retweet_widget.dart
@@ -1,12 +1,13 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:cheep_for_twitter/twitterapi.dart';
 
 class RetweetWidget extends StatefulWidget {
 
-  var isRetweeted, retweetCount;
+  var isRetweeted, retweetCount, id;
 
-  RetweetWidget({Key key, @required this.isRetweeted, @required this.retweetCount}):super(key: key);
+  RetweetWidget({Key key, @required this.isRetweeted, @required this.retweetCount, this.id}):super(key: key);
 
   @override
   State<StatefulWidget> createState() => _RetweetWidgetState();
@@ -39,13 +40,27 @@ class _RetweetWidgetState extends State<RetweetWidget> {
   _toggleRetweet(){
     setState(() {
      if (widget.isRetweeted) {
+        _unretweet();
         widget.retweetCount -= 1;
         widget.isRetweeted = false;
       } else {
+        _retweet();
         widget.retweetCount += 1;
         widget.isRetweeted = true;
       } 
     });
+  }
+  
+  Future<dynamic> _retweet() async {
+    var client = Twitterapi().getClient();
+    return await client.post(
+        'https://api.twitter.com/1.1/statuses/retweet/'+widget.id+'.json');
+  }
+
+  Future<dynamic> _unretweet() async {
+    var client = Twitterapi().getClient();
+    return await client.post(
+        'https://api.twitter.com/1.1/statuses/unretweet/'+widget.id+'.json');
   }
 
 }
