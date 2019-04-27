d9ebd74d79f1f9927582da61bf02933f07de80cc
Kafeel <ahmadkafeel94@gmail.com>
2019-04-27 18:23:11 +0100

    Removed unnecessary variable

--- lib/tweet/reply_widget.dart
+++ lib/tweet/reply_widget.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:cheep_for_twitter/pages/new_tweet.dart';
 
 class ReplyWidget extends StatefulWidget {
-  var isReplied, userName, id;
+  var userName, id;
 
-  ReplyWidget({Key key, @required this.isReplied, this.id, this.userName}) : super(key: key);
+  ReplyWidget({Key key, this.id, this.userName})
+      : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _ReplyWidgetState();
@@ -19,23 +20,16 @@ class _ReplyWidgetState extends State<ReplyWidget> {
             child: IconButton(
           icon: Icon(Icons.reply),
           iconSize: 18,
-          color: (widget.isReplied ? Colors.blue : Colors.grey),
-          onPressed: _toggleReply,
+          color: Colors.grey,
+          onPressed: _reply,
         )),
       ],
     );
   }
 
-  _toggleReply() {
-    setState(() {
-      if (widget.isReplied) {
-        widget.isReplied = false;
-      } else {
-        Navigator.push(context, MaterialPageRoute(builder: (_) {
-          return NewTweet(replyId: widget.id, username: widget.userName);
-        }));
-        widget.isReplied = true;
-      }
-    });
+  _reply() {
+    Navigator.push(context, MaterialPageRoute(builder: (_) {
+      return NewTweet(replyId: widget.id, username: widget.userName);
+    }));
   }
 }
