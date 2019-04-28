ce51d56818768eba915c740d91c945276f0db59e
Kafeel <ahmadkafeel94@gmail.com>
2019-04-28 16:55:32 +0100

    Added comments

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,38 @@ import 'package:cheep_for_twitter/twitterapi.dart';
 import 'package:cheep_for_twitter/tab_bar_home.dart';
 import 'package:cheep_for_twitter/login.dart';
 
+// Create a twitter api instance
 Twitterapi twitterApi = new Twitterapi();
 
 void main() async {
+  // variable that acts a function that takes screen as a parameter
   var lauchScreen = (screen) {
     return runApp(
       MaterialApp(
+          // Title of the app
           title: 'Cheep for Twitter',
           theme: new ThemeData(primaryColor: Colors.blue),
           home: screen),
     );
   };
 
+  // Get the credentials saved from the device
   var credentials = await _getCredentials();
+
+  // if the credentials does exist
   if (credentials != null) {
-  var r = credentials;
-  var r2 = r.split('=');
-  var r3 = r2[1].split('&');
-  var oauthTokenSecret = r2[2];
-  var oauthToken = r3[0];
-  twitterApi.getAuthorClient(oauthToken, oauthTokenSecret);
-  if (credentials != null)
+    var r = credentials;
+    var r2 = r.split('=');
+    var r3 = r2[1].split('&');
+    var oauthTokenSecret = r2[2];
+    var oauthToken = r3[0];
+    // Pass the credentials to the Twitterapi
+    // so it can create a client
+    twitterApi.getAuthorClient(oauthToken, oauthTokenSecret);
+    // launches the TabBarHome screen
     lauchScreen(TabBarHome());
   } else
+    // if the credentials does not exist then it open the Login page
     lauchScreen(Login());
 }
 
